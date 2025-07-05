9d167b040849c2c9541fb21cdc93f97d848646ce
hybk123 <168185868+hybk123@users.noreply.github.com>
2025-07-05 17:08:37 +0800

    Update kv-const.js

--- mail-worker/src/const/kv-const.js
+++ mail-worker/src/const/kv-const.js
@@ -1,4 +1,8 @@
-export const kvConst = {
-  // 验证码 KV 前缀，用于存储邮箱验证码
-  CODE_PREFIX: 'VC_'  // Verification Code
-};
+const KvConst = {
+	AUTH_INFO: 'auth-uid:',
+	SETTING: 'setting:',
+	SEND_DAY_COUNT: 'send_day_count:',
+	CODE_PREFIX: 'VC_'  // 验证码前缀
+}
+
+export default KvConst;
